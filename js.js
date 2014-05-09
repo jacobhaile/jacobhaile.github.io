89e219ae48b75475623f668cca10211c243c1e2d
Jacob Haile <jacobhaile@gmail.com>
2014-05-09 10:51:09 -0700

    Removed html from js file

--- js.js
+++ js.js
@@ -4,52 +4,9 @@ var oneThousand = true;
 var sections = $('ul.sections li');
 var current;
 var others;
-var aboutMe = "<h2>About Me</h2>" +
-              "I'm Jacob Haile, a web developer for hire. I like pizza, " + 
-              "but I love coding. Some of the languages that take up most of my time " +
-              "are Ruby on Rails, Javascript, HTML/CSS/Jquery, and Backbone.js.";
-var contactMe = "<h2>Contact Me</h2>" + 
-                "<a href=\"mailto:jacobhaile@gmail.com\">jacobhaile@gmail.com</a><br>" + 
-                "<a href=\"http://www.linkedin.com/in/jacobhaile\">LinkedIn</a><br>" + 
-                "<a href=\"http://www.github.com/jacobhaile\">Github</a><br>" +
-                "<a href=\"https://www.dropbox.com/s/svkgvmll572xxev/Jacob_Haile_Resume.pdf\">My Resume</a>";
-var portfolio = "<h2>My Projects</h2>" +
-                "<div style=\"width:100%;\">" +
-                  "<div style=\"display:inline-block; float:left; width:30%;\">" +
-                    "<a style=\"color:blue; text-align:left;\" href=\"http://pictur.herokuapp.com/\">Pictur</a>" + 
-                  "</div>" +
-                  "<div style=\"display:inline-block; overflow:hidden; width:70%; text-align:left; line-height:150%;\">" +
-                  "Social image sharing site with commenting, favoriting, and " + 
-                  "voting using AJAX, incorporating extensive CSS and a focus on the finer detail." +
-                  "</div>" +
-                  
-                  "<br>" +
-                  
-                  "<div style=\"display:inline-block; float:left; width:30%;\">" +
-                    "<a style=\"color:blue; text-align:left;\" href=\"http://jacobhaile.github.io/asteroids/\">Asteroids</a>" + 
-                  "</div>" +
-                  "<div style=\"display:inline-block; overflow:hidden; width:70%; text-align:left; line-height:150%;\">" +
-                  "Browser playable arcade game, developed using JS and HTML" + 
-                  "Canvas. Uses clever mathematical calculations and vectors to" + 
-                  "determine momentum and trajectories of asteroids and ships. WASD to move, P to shoot." +
-                  "</div>" +
-                  
-                  "<br>" +
-                  
-                  "<div style=\"display:inline-block; float:left; width:30%;\">" +
-                    "<a style=\"color:blue; text-align:left;\" href=\"https://github.com/zh2009/CHIPS\">CHIPS</a>" + 
-                  "</div>" +
-                  "<div style=\"display:inline-block; overflow:hidden; width:70%; text-align:left; line-height:150%;\">" +
-                  "A team made Android application built to keep track of a" + 
-                  "user’s dietary consumption habits and offers meal suggestions" + 
-                  " adhering to user selected health goals based on ingredients" + 
-                  " the user possesses." +
-                  "</div>" +
-                  
-                "</div>";
-                
-                  
-                
+var aboutMe = $('#about-me').html();
+var portfolio = $('#portfolio').html();
+var contactMe = $('#contact-me').html();
 
 sections.on('click', function() {
   if (!$(this).hasClass('current')) {
@@ -87,6 +44,5 @@ function fillContent() {
     break; 
   }
 }
-
 
 });
